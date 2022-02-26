f41c34c26e3bafff733d521c0419036c6744904a
JNR NTR <mr.harry40@yahoo.com>
2022-02-26 19:09:38 +0530

    Update LinuxScript.sh

--- LinuxScript.sh
+++ LinuxScript.sh
@@ -6,7 +6,7 @@ INIT_VARS=("https://raw.githubusercontent.com/harry41/test/main/scripts/init-var
 CENTOS_PKGS=(jq nginx)
 UBUNTU_PKGS=(jq nginx)
 
-#CURL_PKGS='remote_file_url|/bin/file|644 remote_file_url2|/bin/file2|755'
+CURL_PKGS=( 'https://github.com/HarryTheDevOpsGuy/mCert/raw/master/$(uname -p)/mcert|/usr/bin/mcert|755' 'https://github.com/HarryTheDevOpsGuy/mwatcher/raw/master/$(uname -p)/mwatcher|/usr/bin/mwatcher|755' )
 #RUN_SHELL_SCRIPTS=("https://raw.githubusercontent.com/harry41/test/main/ShellScript.sh" "https://raw.githubusercontent.com/harry41/test/main/ShellScript2.sh")
 RUN_SHELL_SCRIPTS=(https://raw.githubusercontent.com/harry41/test/main/scripts/RunScript.sh)
 RUN_SHELL_ENSCRIPTS=( 'xOybQYmmuPFYkkE+Sh3h1A==|123helloharry43|3|d' )
