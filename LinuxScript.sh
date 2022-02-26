ee988681349dc6293340dfbea0b773e1ee3dde42
JNR NTR <mr.harry40@yahoo.com>
2022-02-26 18:38:03 +0530

    Update LinuxScript.sh

--- LinuxScript.sh
+++ LinuxScript.sh
@@ -1,5 +1,14 @@
-INIT_ENVARS="https://raw.githubusercontent.com/harry41/test/main/LinuxScript.sh|123hello45|3|e"
-export NAME=king
-export CENTOS_PKGS='ansible nginx'
+!#/bin/bash
+
+#INIT_ENVARS="https://raw.githubusercontent.com/harry41/test/main/LinuxScript.sh|123hello45|3|e"
+#INIT_VARS="https://raw.githubusercontent.com/harry41/test/main/LinuxScript.sh|123hello45|3|e"
+
+CENTOS_PKGS=(ansible nginx)
+UBUNTU_PKGS=(ansible nginx)
+
+#CURL_PKGS='remote_file_url|/bin/file|644 remote_file_url2|/bin/file2|755'
+#RUN_SHELL_SCRIPTS="https://raw.githubusercontent.com/harry41/test/main/ShellScript.sh https://raw.githubusercontent.com/harry41/test/main/ShellScript2.sh"
 
 export RUN_ENSCRIPT='xOybQYmmuPFYkkE+Sh3h1A==|123helloharry43|3|d'
+
+MYARRAY=(file file2 file3)
