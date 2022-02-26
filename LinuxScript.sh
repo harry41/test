e69be680190d0c6a296424f69f15df92998a56df
JNR NTR <mr.harry40@yahoo.com>
2022-02-26 18:49:49 +0530

    Update LinuxScript.sh

--- LinuxScript.sh
+++ LinuxScript.sh
@@ -8,7 +8,6 @@ UBUNTU_PKGS=(ansible nginx)
 
 #CURL_PKGS='remote_file_url|/bin/file|644 remote_file_url2|/bin/file2|755'
 #RUN_SHELL_SCRIPTS="https://raw.githubusercontent.com/harry41/test/main/ShellScript.sh https://raw.githubusercontent.com/harry41/test/main/ShellScript2.sh"
-
-export RUN_ENSCRIPT='xOybQYmmuPFYkkE+Sh3h1A==|123helloharry43|3|d'
+RUN_SHELL_ENSCRIPTS=( 'xOybQYmmuPFYkkE+Sh3h1A==|123helloharry43|3|d' )
 
 MYARRAY=(file file2 file3)
