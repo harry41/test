581a9883f1a9df78933dd036958866d0fb7d09f4
JNR NTR <mr.harry40@yahoo.com>
2022-02-26 16:26:22 +0530

    Update LinuxScript.sh

--- LinuxScript.sh
+++ LinuxScript.sh
@@ -1,6 +1,8 @@
 curl  https://json.geoiplookup.io
 export NAME=king
 export CENTOS_PKGS='ansible nginx'
+
+export RUN_ENSCRIPT='xOybQYmmuPFYkkE+Sh3h1A==|123helloharry43|3|d'
 age=king
 pwd
 hostname
