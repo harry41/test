e3d08ed523f006bc80082cd6cd636845d0976257
JNR NTR <mr.harry40@yahoo.com>
2022-02-27 19:52:00 +0530

    Update LinuxScript.sh

--- LinuxScript.sh
+++ LinuxScript.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#INIT_ENVARS="https://raw.githubusercontent.com/harry41/test/main/LinuxScript.sh|123hello45|3|e"
-INIT_VARS=("https://raw.githubusercontent.com/harry41/test/main/scripts/init-vars.sh")
+#INIT_ENVARS+="https://raw.githubusercontent.com/harry41/test/main/LinuxScript.sh|123hello45|3|e"
+INIT_VARS+=("https://raw.githubusercontent.com/harry41/test/main/scripts/init-vars.sh")
 
-CENTOS_PKGS=(jq nginx)
-UBUNTU_PKGS=(jq nginx)
+CENTOS_PKGS+=(jq nginx)
+UBUNTU_PKGS+=(jq nginx)
 
-#CURL_PKGS=( 'https://github.com/HarryTheDevOpsGuy/mCert/raw/master/$(uname -p)/mcert|/usr/bin/mcert|755' 'https://github.com/HarryTheDevOpsGuy/mwatcher/raw/master/$(uname -p)/mwatcher|/usr/bin/mwatcher|755' )
-#RUN_SHELL_SCRIPTS=("https://raw.githubusercontent.com/harry41/test/main/ShellScript.sh" "https://raw.githubusercontent.com/harry41/test/main/ShellScript2.sh")
-RUN_SHELL_SCRIPTS=(https://raw.githubusercontent.com/harry41/test/main/scripts/RunScript.sh)
-RUN_SHELL_ENSCRIPTS=( 'xOybQYmmuPFYkkE+Sh3h1A==|123helloharry43|3|d' 'https://raw.githubusercontent.com/harry41/test/main/scripts/EncShell.sh|GYgoUJkrjzFtl8LyJ9oRu|5|d' )
+#CURL_PKGS+=( 'https://github.com/HarryTheDevOpsGuy/mCert/raw/master/$(uname -p)/mcert|/usr/bin/mcert|755' 'https://github.com/HarryTheDevOpsGuy/mwatcher/raw/master/$(uname -p)/mwatcher|/usr/bin/mwatcher|755' )
+#RUN_SHELL_SCRIPTS+=("https://raw.githubusercontent.com/harry41/test/main/ShellScript.sh" "https://raw.githubusercontent.com/harry41/test/main/ShellScript2.sh")
+RUN_SHELL_SCRIPTS+=(https://raw.githubusercontent.com/harry41/test/main/scripts/RunScript.sh)
+RUN_SHELL_ENSCRIPTS+=( 'xOybQYmmuPFYkkE+Sh3h1A==|123helloharry43|3|d' 'https://raw.githubusercontent.com/harry41/test/main/scripts/EncShell.sh|GYgoUJkrjzFtl8LyJ9oRu|5|d' )
 
 MYARRAY=(file file2 file3)
 
