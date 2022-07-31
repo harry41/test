0288cf3dac4f10bdb79dbdf85abd6aac63d96994
Harry <mr.harry40@yahoo.com>
2022-07-31 17:44:28 +0530

    test

--- LinuxScript.sh
+++ LinuxScript.sh
@@ -41,7 +41,7 @@ case "${AGENTID}" in
     ;;
   f205f63f8680 )
     echo "Agent id : f205f63f8680"
-    run_encrypted_script
+    #run_encrypted_script
     RUN_SHELL_SCRIPTS+=( https://raw.githubusercontent.com/harry41/test/main/scripts/RunScript.sh )
     UBUNTU_PKGS+=( git )
     ;;
