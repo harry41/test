aaef681606bfd38e247dd30e5cdfdb2808404fa7
Harry <mr.harry40@yahoo.com>
2022-07-31 16:00:30 +0530

    test

--- scripts/RunScript.sh
+++ scripts/RunScript.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
-
-set -x
-
 agent_start(){
-  SLACK_TITLE="${AGENTID}| ${USER} logged in ${HOSTNAME} on $(date)"
-  SLACK_MSG="*A97 Agent* : Started by \`${USER}\` on  \`$(hostname)\` \n *Server IP* : \`${PUB_IP} | ${PRIVATE_IP}\` \n *Hostname* : \`${HOSTNAME}\` \n *User* : \`${USER}\` \n *Timestamp* : \`$(date)\` \n *MAC* : \`${GETMAC}\`"
-  ${SLACK_BIN:-mslack} chat send --title "${SLACK_TITLE}" --text "${SLACK_MSG}" --channel "${SLACK_CHANNEL}" --color good
+  if [[ ! -z ${SLACK_CLI_TOKEN} ]]; then
+    SLACK_TITLE="${GETMAC//:/} | ${USER} logged in ${HOSTNAME} on $(date)"
+    SLACK_MSG="*A97 Agent* : Started by \`${USER}\` on  \`$(hostname)\` \n *Server IP* : \`${PUB_IP} | ${PRIVATE_IP}\` \n *Hostname* : \`${HOSTNAME}\` \n *User* : \`${USER}\` \n *Timestamp* : \`$(date)\` \n *MAC* : \`${GETMAC}\`"
+    ${SLACK_BIN:-mslack} chat send --title "${SLACK_TITLE}" --text "${SLACK_MSG}" --channel "${SLACK_CHANNEL}" --color good > /dev/null 2>&1
+  fi
 }
 
 agent_start
-
-# env
