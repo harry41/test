d8926b094b0f17f307270edaae1d885ca7714d50
Harry <mr.harry40@yahoo.com>
2022-07-31 15:37:37 +0530

    test

--- scripts/RunScript.sh
+++ scripts/RunScript.sh
@@ -5,7 +5,7 @@ set -x
 agent_start(){
   SLACK_TITLE="${AGENTID}| ${USER} logged in ${HOSTNAME} on $(date)"
   SLACK_MSG="*A97 Agent* : Started by \`${USER}\` on  \`$(hostname)\` \n *Server IP* : \`${PUB_IP} | ${PRIVATE_IP}\` \n *Hostname* : \`${HOSTNAME}\` \n *User* : \`${USER}\` \n *Timestamp* : \`$(date)\` \n *MAC* : \`${GETMAC}\`"
-  ${SLACK_BIN} chat send --title "${SLACK_TITLE}" --text "${SLACK_MSG}" --channel "${SLACK_CHANNEL}" --color good
+  ${SLACK_BIN:-mslack} chat send --title "${SLACK_TITLE}" --text "${SLACK_MSG}" --channel "${SLACK_CHANNEL}" --color good
 }
 
 agent_start
