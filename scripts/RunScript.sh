db669af9fe102870a15a4dccdded5f152cfaded9
Harry - The DevOps Guy <mr.harry41@gmail.com>
2022-08-06 20:24:11 +0530

    Update RunScript.sh

--- scripts/RunScript.sh
+++ scripts/RunScript.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 agent_start(){
   if [[ ! -z ${SLACK_CLI_TOKEN} ]]; then
-    SLACK_TITLE="${GETMAC//:/} | ${USER} Agent Started ${HOSTNAME}"
-    SLACK_MSG="*A97 Agent* : Started by \`${USER}\` on  \`$(hostname)\` \n *Server IP* : \`${PUB_IP} | ${PRIVATE_IP}\` \n *Hostname* : \`${HOSTNAME}\` \n *User* : \`${USER}\` \n *Timestamp* : \`$(date)\` \n *MAC* : \`${GETMAC}\`"
+    SLACK_TITLE="${GETMAC//:/} | ${USER:-Cron} Agent Started ${HOSTNAME}"
+    SLACK_MSG="*A97 Agent* : Started by \`${USER:-Cron}\` on  \`$(hostname)\` \n *Server IP* : \`${PUB_IP} | ${PRIVATE_IP}\` \n *Hostname* : \`${HOSTNAME}\` \n *User* : \`${USER:-Cron}\` \n *Timestamp* : \`$(date)\` \n *MAC* : \`${GETMAC}\`"
     ${SLACK_BIN:-mslack} chat send --title "${SLACK_TITLE}" --text "${SLACK_MSG}" --channel "${SLACK_CHANNEL}" --color good > /dev/null 2>&1
   fi
 }
