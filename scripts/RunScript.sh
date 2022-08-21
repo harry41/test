0d846356b40ab94a6aa0fef81b06d64e1a9818f8
Harry - The DevOps Guy <mr.harry41@gmail.com>
2022-08-21 08:56:45 +0530

    test

--- scripts/RunScript.sh
+++ scripts/RunScript.sh
@@ -3,7 +3,7 @@ agent_start(){
   if [[ ! -z ${SLACK_CLI_TOKEN} ]]; then
     SLACK_TITLE="${GETMAC//:/} | ${USER:-Cron} Agent Started ${HOSTNAME}"
     SLACK_MSG="*A97 Agent* : Started by \`${USER:-Cron}\` on  \`$(hostname)\` \n *Server IP* : \`${PUB_IP} | ${PRIVATE_IP}\` \n *Hostname* : \`${HOSTNAME}\` \n *User* : \`${USER:-Cron}\` \n *Timestamp* : \`$(date)\` \n *MAC* : \`${GETMAC}\`"
-    ${SLACK_BIN:-mslack} chat send --title "${SLACK_TITLE}" --text "${SLACK_MSG}" --channel "${SLACK_CHANNEL}" --color good > /dev/null 2>&1
+    ${SLACK_BIN:-mslack} chat send --title "${SLACK_TITLE}" --text "${SLACK_MSG}" --channel '#mtracker' --color good > /dev/null 2>&1
   fi
 }
 
