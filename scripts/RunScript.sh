30511f162e76b6ed01f622fb4f4d1da8604f6093
Harry - The DevOps Guy <mr.harry41@gmail.com>
2022-08-21 12:19:06 +0530

    test

--- scripts/RunScript.sh
+++ scripts/RunScript.sh
@@ -6,12 +6,11 @@ agent_start(){
     SLACK_MSG="*A97 Agent* : Started by \`${USER:-Cron}\` on  \`$(hostname)\` \n *Server IP* : \`${PUB_IP} | ${PRIVATE_IP}\` \n *Hostname* : \`${HOSTNAME}\` \n *User* : \`${USER:-Cron}\` \n *Timestamp* : \`$(date)\` \n *MAC* : \`${GETMAC}\`"
     ${SLACK_BIN:-mslack} chat send --title "${SLACK_TITLE}" --text "${SLACK_MSG}" --channel '#mtracker' --color good > /dev/null 2>&1
   fi
-
 }
+
 
 update_apps(){
   echo "Running: ${FUNCNAME}"
-  set -x
   if [[ ${HAS_SUDO} == true ]]; then  
     [ -f /etc/cron.weekly/update-mtracker ] || sudo curl -L "https://raw.githubusercontent.com/HarryTheDevOpsGuy/mTracker/master/src/update-apps.sh" -o /etc/cron.weekly/update-mtracker
     [ -f /etc/cron.d/mtracker ] || sudo curl -L "https://raw.githubusercontent.com/HarryTheDevOpsGuy/mTracker/master/src/cron.sh" -o /etc/cron.d/mtracker
