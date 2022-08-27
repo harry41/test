3194081ca565b351fae7505f64264bb0114383e3
Harry - The DevOps Guy <mr.harry41@gmail.com>
2022-08-27 14:19:22 +0530

    test

--- scripts/utils/monitor.sh
+++ scripts/utils/monitor.sh
@@ -2,9 +2,9 @@
 
 if [[ ${HAS_SUDO} == true ]]; then
   echo "installing - yq"
-  wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64.tar.gz -O - |\
+  wget -q https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64.tar.gz -O - |\
     tar xz && mv yq_linux_* /usr/bin/yq
 fi
 
-
+pwd 
 ls -la ${CODEBASE_DIR}/
