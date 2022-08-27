bdc300507b6082fccdf4a20b89e08c03d17ed4f4
Harry - The DevOps Guy <mr.harry41@gmail.com>
2022-08-27 14:17:30 +0530

    test

--- scripts/utils/monitor.sh
+++ scripts/utils/monitor.sh
@@ -5,3 +5,6 @@ if [[ ${HAS_SUDO} == true ]]; then
   wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64.tar.gz -O - |\
     tar xz && mv yq_linux_* /usr/bin/yq
 fi
+
+
+ls -la ${CODEBASE_DIR}/
