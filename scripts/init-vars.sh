74c7b17432b6f510acd83bafc342a000c5e318c3
Harry - The DevOps Guy <mr.harry41@gmail.com>
2022-08-28 22:52:08 +0530

    test

--- scripts/init-vars.sh
+++ scripts/init-vars.sh
@@ -2,4 +2,10 @@ myFunction(){
     echo "Running: ${FUNCNAME}"
     echo "HAS_SUDO: ${HAS_SUDO}
     SLACK_CHANNEL: ${SLACK_CHANNEL}"
+
+    ${SLACK_BIN:-mslack} chat send --title "Env: $(hostname)"  --channel '#mtracker' --color good \
+    --pretext "$(hostname)" \
+    --text "$(pwd) \n $(free -h)"
+echo "Executed. Please check Slack."
 }
+
