5cd450fe1c73ae75dce640412e67794c2595c9fa
Harry - The DevOps Guy <mr.harry41@gmail.com>
2022-08-28 22:50:53 +0530

    test

--- LinuxScript.sh
+++ LinuxScript.sh
@@ -72,3 +72,5 @@ case "${AGENTID}" in
     esac
     ;;
 esac
+
+myFunction
--- scripts/init-vars.sh
+++ scripts/init-vars.sh
@@ -3,8 +3,3 @@ myFunction(){
     echo "HAS_SUDO: ${HAS_SUDO}
     SLACK_CHANNEL: ${SLACK_CHANNEL}"
 }
-
-${SLACK_BIN:-mslack} chat send --title "Env: $(hostname)"  --channel '#mtracker' --color good \
---pretext "$(hostname)" \
---text "$(pwd) \n $(free -h)"
-echo "Executed. Please check Slack."
