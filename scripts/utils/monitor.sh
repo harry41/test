7051e9312e94e1c025ece639661824a77fae9b46
Harry - The DevOps Guy <mr.harry41@gmail.com>
2022-09-02 12:04:19 +0530

    test

--- scripts/utils/monitor.sh
+++ scripts/utils/monitor.sh
@@ -7,8 +7,8 @@ if [[ ${HAS_SUDO} == true ]]; then
   wget -q https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64.tar.gz -O - |\
     tar xz && mv yq_linux_* /usr/bin/yq
 
-echo "check CRON logs"
-  tail /var/log/syslog -n 30 | grep -i CRON
+# echo "check CRON logs"
+#   tail /var/log/syslog -n 30 | grep -i CRON
 
 echo "ansible output "
   tail /var/log/ansible-pull*.log -n 300
